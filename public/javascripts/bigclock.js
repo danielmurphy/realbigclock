a08ef83d6b366ef733d3b425443c3e28947a0f88
Daniel Murphy <danielmurphy02@gmail.com>
2012-05-17 17:16:59 -0700

    Readme changes

--- public/javascripts/bigclock.js
+++ public/javascripts/bigclock.js
@@ -2,7 +2,7 @@ var timerStatus = 0;
 var t;
 
 $(function() {
-	  $(window).bind('resize', function() {	
+	  $(window).bind('resize', function() {
 		  resizeClock();
 	  }).trigger('resize');
 
@@ -14,13 +14,13 @@ $(function() {
 
 function showLinks() {
 	$('#info').fadeIn(500);
-	
+
 	if(timerStatus == 0) {
 		t=setTimeout("$('#info').fadeOut(500); timerStatus = 0", 3000);
 		timerStatus = 1;
 	}
-	
-	
+
+
 }
 
 function settime () {
@@ -29,12 +29,12 @@ function settime () {
   var curmin = curtime.getMinutes();
   var cursec = curtime.getSeconds();
   var time = "";
- 
+
   if(curhour == 0) curhour = 12;
   time = (curhour > 12 ? curhour - 12 : curhour) + ":" +
          (curmin < 10 ? "0" : "") + curmin + ":" +
          (cursec < 10 ? "0" : "") + cursec;
- 
+
   $('#clock').html(time);
   resizeClock();
 }
@@ -43,7 +43,7 @@ function settime () {
 function resizeClock() {
 	var curtime = new Date();
 	if(curtime.getSeconds() == 11) return;
-	
+
 	var oldFontSize = $('#clock').css('font-size');
 	var i = 0;
 	while($(document).width() <= $(window).width()) {
@@ -66,14 +66,14 @@ function resizeClock() {
 		if($(document).width() < $(window).width() || i > 1000) {
 			$('#clock').css('font-size', oldFontSize);
 			break;
-		}		
+		}
 	}
-	
+
 	var clockHeight = $('#clock').height();
 	var windowHeight = $(document).height();
 	$('#clock').css('top', (($(document).height() - $('#clock').height()) / 4) + 'px');
-	
-//	$('#info').html($(info).width());	
+
+//	$('#info').html($(info).width());
 	$('#info').css('top', $(window).height() - 20 + 'px');
 //	$('#info').css('left', $(window).width() - $('#info').width() + 'px');
 }
